0cc8635b2f3bc7983066b2f937b5d813098733d3
dimasjp <diims.el@gmail.com>
2022-03-10 09:15:24 +0700

    Function playRound first draft
    Added more to function playRound,
    Changed choices from const to let.

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+
 function computerPlay() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    let result = ""
+    if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
+        (playerSelection == 'Paper' && computerSelection == 'Rock') ||
+        (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
+            return ('You Win! ' + playerSelection + ' beats ' + computerSelection)
+    }
 
-    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-        (playerSelection == 'paper' && computerSelection == 'rock') ||
-        (playerSelection == 'scissors' && computerSelection == 'paper')) {
-            result = 'You Win!' + playerSelection + 'beats' + computerSelection
+    else if ((playerSelection == 'Rock' && computerSelection == 'Paper') ||
+    (playerSelection == 'Paper' && computerSelection == 'Scissors') ||
+    (playerSelection == 'Scissors' && computerSelection == 'Rock')) {
+        return ('You Lose! ' + computerSelection + ' beats ' + playerSelection)
+}
+
+    else if (playerSelection == computerSelection); {
+        return ('Draw!')
     }
 }
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
