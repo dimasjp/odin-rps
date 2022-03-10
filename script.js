eac808be435307ec8e4ee8ecac7bf13f68105de9
diimsel <diims.el@gmail.com>
2022-03-10 07:42:11 +0700

    Add unfinised function playRound

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function computerPlay() {
     const choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    let result = ""
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper')) {
+            result = 'You Win!' + playerSelection + 'beats' + computerSelection
+    }
+}
