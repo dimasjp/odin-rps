a4e350ecf79c530c13ae98db5d1cfdeed3007e18
dimsjp.win <diims.el@gmail.com>
2022-04-03 13:16:48 +0700

    Add game over condition and score display

--- script.js
+++ script.js
@@ -1,42 +1,61 @@
+//DOM Select
+const btnRock = document.querySelector('.btnRock');
+const btnPaper = document.querySelector('.btnPaper');
+const btnScissors = document.querySelector('.btnScissors');
+const result = document.querySelector('#result');
+const title = document.querySelector('#title');
+const resultFinal = document.querySelector('#resultFinal');
+const scores = document.querySelector('#scores');
+
+
 let playerScore = 0;
 let computerScore = 0;
-
 
 //Computer play random RPS
 function computerPlay() {
     let choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
-}
+};
 
 //Play one round of RPS
 function playRound(playerSelect, computerSelect) {
     let roundResult = "";
+    title.remove();
 
-    if ((playerSelect == 'Rock' && computerSelect == 'Scissors') ||
-        (playerSelect == 'Paper' && computerSelect == 'Rock') ||
-        (playerSelect == 'Scissors' && computerSelect == 'Paper')) {
+    if ((playerSelect === 'Rock' && computerSelect === 'Scissors') ||
+        (playerSelect === 'Paper' && computerSelect === 'Rock') ||
+        (playerSelect === 'Scissors' && computerSelect === 'Paper')) {
             roundResult = 'playerWin';
             playerScore += 1;
+            if (playerScore === 5) {
+                gameOver(playerScore, computerScore);
+                btnRock.removeEventListener('click', () => playerChoose('Rock'));
+                btnPaper.removeEventListener('click', () => playerChoose('Paper'));
+                btnScissors.removeEventListener('click', () => playerChoose('Scissors'));
+            }
     }
 
-    else if ((playerSelect == 'Rock' && computerSelect == 'Paper') ||
-        (playerSelect == 'Paper' && computerSelect == 'Scissors') ||
-        (playerSelect == 'Scissors' && computerSelect == 'Rock')) {
+    else if ((playerSelect === 'Rock' && computerSelect === 'Paper') ||
+        (playerSelect === 'Paper' && computerSelect === 'Scissors') ||
+        (playerSelect === 'Scissors' && computerSelect === 'Rock')) {
             roundResult = 'computerWin';
             computerScore += 1;
+            if (computerScore === 5) {
+                gameOver(playerScore, computerScore);
+                btnRock.removeEventListener('click', () => playerChoose('Rock'));
+                btnPaper.removeEventListener('click', () => playerChoose('Paper'));
+                btnScissors.removeEventListener('click', () => playerChoose('Scissors'));
+            }
     }
 
     else {
         roundResult = 'draw';
     }
     resultText(roundResult, playerSelect, computerSelect);
-}
+    scoreDisplay(playerScore, computerScore);
+};
 
-//Button
-const btnRock = document.querySelector('#btnRock');
-const btnPaper = document.querySelector('#btnPaper');
-const btnScissors = document.querySelector('#btnScissors');
-
+//Button Event
 btnRock.addEventListener('click', () => playerChoose('Rock'));
 btnPaper.addEventListener('click', () => playerChoose('Paper'));
 btnScissors.addEventListener('click', () => playerChoose('Scissors'));
@@ -47,8 +66,6 @@ function playerChoose(playerSelect) {
 }
 
 //Text Content
-const result = document.querySelector('#result');
-
 function resultText(roundResult, playerSelect, computerSelect) {
     if (roundResult === 'playerWin') {
         result.textContent = ('You Win! ' + playerSelect + ' beats ' + computerSelect);
@@ -61,9 +78,22 @@ function resultText(roundResult, playerSelect, computerSelect) {
     else {
         result.textContent = ('Draw!');
     }
+};
+
+function scoreDisplay(playerScore, computerScore) {
+    scores.textContent = (playerScore + ' vs ' + computerScore);
 }
 
+function gameOver(playerScore, computerScore) {
+    result.remove();
+    if (playerScore === 5) {
+        resultFinal.textContent = ('🎉You Win!');
+    }
 
+    else if (computerScore === 5) {
+        resultFinal.textContent = ('🤖You Lose!');
+    }
+};
 
 // //play 5 round of rps
 // function game() {
